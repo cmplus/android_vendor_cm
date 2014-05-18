eeff47b6d9e8f5fdfa7d29b1694e393b07fedf18
sspencer10 <sspencer10@gmail.com>
2014-05-18 02:42:08 -0500

    changelog

--- config/common.mk
+++ config/common.mk
@@ -76,7 +76,7 @@ endif
 
 # Copy over the changelog to the device
 PRODUCT_COPY_FILES += \
-    vendor/cm/CHANGELOG.mkdn:system/etc/CHANGELOG-CM.txt
+    vendor/cm/tools/CHANGELOG-CM.txt:system/etc/CHANGELOG-CM.txt
 
 # Backup Tool
 ifneq ($(WITH_GMS),true)
